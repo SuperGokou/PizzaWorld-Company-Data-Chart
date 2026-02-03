94788baaad7ddcc31c1a58d2fa5f14e4a0ca95c6
Ming Xia <githubming@hotmail.com>
2026-02-02 22:47:35 -0800

    Revamp CSS/README, remove glyphicons
    Add .gitignore and refactor presentation and assets: rewrite css/style.css with a full style overhaul (fonts, cards, filters, table, chart, tooltip, divider) to modernize layout and visuals; update README.md formatting and project-structure section for clarity; apply related updates to index.html and JS modules (js/barchart.js, js/main.js) to match the new styles and interactions. Remove unused glyphicons font files to reduce repo bloat. .gitignore added to ignore IDE files, OS artifacts and node_modules.

--- js/barchart.js
+++ js/barchart.js
@@ -1,17 +1,11 @@
-
 /*****************************************/
 /*   DRAW BAR CHART - ALREADY COMPLETE   */
 /*****************************************/
 
-
 // Chart area
-
-let margin = {top: 60, right: 20, bottom: 40, left: 80},
-    width = document.getElementById('chart-area').getBoundingClientRect().width - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
-
-width = width > 600 ? 600 : width;
-width = width < 400 ? 400 : width;
+let margin = {top: 40, right: 20, bottom: 40, left: 50},
+    width = 580,
+    height = 320;
 
 let svg = d3.select("#chart-area").append("svg")
     .attr("width", width + margin.left + margin.right)
@@ -19,10 +13,8 @@ let svg = d3.select("#chart-area").append("svg")
     .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-
 // Parse date
 let parseDate = d3.timeParse("%Y-%m-%d");
-
 
 // Scales and axes
 let x = d3.scaleBand()
@@ -43,16 +35,10 @@ let xAxisGroup = svg.append("g")
     .attr("class", "x-axis axis");
 
 let yAxisGroup = svg.append("g")
-  .attr("class", "y-axis axis");
+    .attr("class", "y-axis axis");
 
-
-// render function
-function renderBarChart (data){
-
-    // logging data
-    console.log('rendering barChart', data)
-
-    // former versions of d3 used 'nesting' which has been superseded by 'grouping', see https://github.com/d3/d3-collection#nests
+// Render function
+function renderBarChart(data) {
     let groupedData = Array.from(d3.group(data, d => d.date), ([key, array]) => ({key: key, value: array.length}));
 
     groupedData.forEach(d => {
@@ -64,7 +50,7 @@ function renderBarChart (data){
     x.domain(groupedData.map(d => d.key));
     y.domain([0, d3.max(groupedData, d => d.value)]);
 
-  // ---- DRAW BARS ----
+    // Draw bars
     let bars = svg.selectAll(".bar")
         .data(groupedData);
 
@@ -79,44 +65,38 @@ function renderBarChart (data){
         .attr("height", d => (height - y(d.value)))
         .attr("width", x.bandwidth())
         .on("mouseover", function(event, d) {
-            // no arrow function used here to have access to 'this'
-            //Get this bar's x/y values, then augment for the tooltip
-            let xPosition = width + parseFloat(d3.select(this).attr("x")) + x.bandwidth() / 2;
-            let yPosition = height*2.5 + parseFloat(d3.select(this).attr("y")) / 2 + height/3;
+            let chartArea = document.getElementById('chart-area');
+            let rect = chartArea.getBoundingClientRect();
+            let xPosition = event.clientX - rect.left + 15;
+            let yPosition = event.clientY - rect.top - 30;
 
-
-            //Update the tooltip position and value
             d3.select("#tooltip")
                 .style("left", xPosition + 'px')
                 .style("top", yPosition + 'px')
                 .select("#value")
                 .text(d.value);
 
-            //Show the tooltip
             d3.select("#tooltip").classed("hidden", false);
         })
-        .on("mouseout", function(d) {
-            //Hide the tooltip
+        .on("mouseout", function() {
             d3.select("#tooltip").classed("hidden", true);
         });
 
+    // Draw axes
+    xAxisGroup = svg.select(".x-axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
 
-  // ---- DRAW AXIS ----
+    yAxisGroup = svg.select(".y-axis")
+        .call(yAxis);
 
-  xAxisGroup = svg.select(".x-axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis);
+    svg.select("text.axis-title").remove();
 
-  yAxisGroup = svg.select(".y-axis")
-      .call(yAxis);
-
-  svg.select("text.axis-title").remove();
-
-  svg.append("text")
-    .attr("class", "axis-title")
-    .attr("x", -5)
-    .attr("y", -15)
-    .attr("dy", ".1em")
-    .style("text-anchor", "end")
-    .text("Deliveries");
+    svg.append("text")
+        .attr("class", "axis-title")
+        .attr("x", -5)
+        .attr("y", -15)
+        .attr("dy", ".1em")
+        .style("text-anchor", "end")
+        .text("Deliveries");
 }
--- js/main.js
+++ js/main.js
@@ -1,148 +1,68 @@
-
-
 // DATASETS
+// Global variable with 1198 pizza deliveries (deliveryData)
+// Global variable with 200 customer feedbacks (feedbackData)
 
-// Global variable with 1198 pizza deliveries
-// console.log(deliveryData);
-
-// Global variable with 200 customer feedbacks
-// console.log(feedbackData.length);
-
-
-// FILTER DATA, THEN DISPLAY SUMMARY OF DATA & BAR CHART
-
+// Initialize visualization on page load
 createVisualization();
 
-function matchFeedback(feedbackDat, deliveryDat){
-
-
-}
-
-
-function switchFunction(area, feedbackDat, deliveryDat){
-    switch (area){
-        case "All":
-            loadTable(feedbackDat, deliveryDat);
-            renderBarChart(deliveryDat);
-            break;
-        case "Boston":
-            let areadeliveryData1 = deliveryDat.filter((value, index) => {
-                return (value.area === "Boston");
-            });
-            let newfeedback1 = feedbackDat.filter(a => areadeliveryData1.some(b => b.delivery_id === a.delivery_id));
-            loadTable(newfeedback1, areadeliveryData1);
-            renderBarChart(areadeliveryData1);
-            break;
-        case "Cambridge":
-            let areadeliveryData2 = deliveryDat.filter((value, index) => {
-                return (value.area === "Cambridge");
-            });
-            let newfeedback2 = feedbackDat.filter(a => areadeliveryData2.some(b => b.delivery_id === a.delivery_id));
-
-            loadTable(newfeedback2, areadeliveryData2);
-            renderBarChart(areadeliveryData2);
-            break;
-        case "Somerville":
-            let areadeliveryData3 = deliveryDat.filter((value, index) => {
-                return (value.area === "Somerville");
-            });
-            let newfeedback3 = feedbackDat.filter(a => areadeliveryData3.some(b => b.delivery_id === a.delivery_id));
-            loadTable(newfeedback3, areadeliveryData3);
-            renderBarChart(areadeliveryData3);
-            break;
+function filterByArea(area, feedbackDat, deliveryDat) {
+    if (area === "All" || area === "hide") {
+        loadTable(feedbackDat, deliveryDat);
+        renderBarChart(deliveryDat);
+        return;
     }
+
+    let filteredDeliveries = deliveryDat.filter(item => item.area === area);
+    let filteredFeedback = feedbackDat.filter(fb =>
+        filteredDeliveries.some(del => del.delivery_id === fb.delivery_id)
+    );
+
+    loadTable(filteredFeedback, filteredDeliveries);
+    renderBarChart(filteredDeliveries);
 }
-function dataFiltering(area, ordertype, deliveryDat, feedbackDat) {
 
-    switch (ordertype){
-        case "All":
-            switchFunction(area, feedbackDat, deliveryDat)
-            break;
-        case "Web":
-            let deliveryDatatype1 = deliveryDat.filter((value, index) => {
-                return (value.order_type === "web");
-            });
-
-            let newfeedback1 = feedbackDat.filter(a => deliveryDatatype1.some(b => b.delivery_id === a.delivery_id));
-            switchFunction(area, newfeedback1, deliveryDatatype1)
-            break;
-        case "Phone":
-            let deliveryDatatype2 = deliveryDat.filter((value, index) => {
-                return (value.order_type === "phone");
-            });
-            let newfeedback2 = feedbackDat.filter(a => deliveryDatatype2.some(b => b.delivery_id === a.delivery_id));
-            switchFunction(area, newfeedback2, deliveryDatatype2)
-            break;
-        default:
-            switchFunction(area, feedbackDat, deliveryDat)
-            break;
+function dataFiltering(area, orderType, deliveryDat, feedbackDat) {
+    if (orderType === "All" || orderType === "hide") {
+        filterByArea(area, feedbackDat, deliveryDat);
+        return;
     }
+
+    let typeKey = orderType.toLowerCase();
+    let filteredDeliveries = deliveryDat.filter(item => item.order_type === typeKey);
+    let filteredFeedback = feedbackDat.filter(fb =>
+        filteredDeliveries.some(del => del.delivery_id === fb.delivery_id)
+    );
+
+    filterByArea(area, filteredFeedback, filteredDeliveries);
 }
-function loadTable(feedbackData, deliveryData){
 
-    let feedbackDat = feedbackData;
-    let deliveryDat = deliveryData;
+function loadTable(feedbackDat, deliveryDat) {
+    let totalPizzas = deliveryDat.reduce((sum, item) => sum + item.count, 0);
+    let totalDeliveryTime = deliveryDat.reduce((sum, item) => sum + item.delivery_time, 0);
+    let avgTime = totalDeliveryTime / deliveryDat.length;
+    let totalSales = deliveryDat.reduce((sum, item) => sum + item.price, 0);
 
-    var count_pizza = 0;
-    deliveryData.forEach(item =>{
-        count_pizza += item.count
-    });
-
-    var sum_del_time = 0;
-    deliveryData.forEach(item =>{
-        sum_del_time += item.delivery_time
-    });
-    var ave_time = sum_del_time/Object.keys(deliveryDat).length;
-
-    var sum_sales = 0;
-    deliveryData.forEach(item =>{
-        sum_sales += item.price
-    });
-
-    let low = feedbackDat.filter( (value, index) => {
-        return (value.quality === 'low');
-    });
-
-    let medium = feedbackDat.filter( (value, index) => {
-        return (value.quality === 'medium');
-    });
-
-    let high = feedbackDat.filter( (value, index) => {
-        return (value.quality === 'high');
-    });
-
+    let lowCount = feedbackDat.filter(item => item.quality === 'low').length;
+    let mediumCount = feedbackDat.filter(item => item.quality === 'medium').length;
+    let highCount = feedbackDat.filter(item => item.quality === 'high').length;
 
     const tableEle = document.getElementById("tableData");
-    let dataHtml = `<tr><td>Number of pizza deliveries</td><td>${Object.keys(deliveryDat).length}</td></tr>`
-    dataHtml += `<tr><td>Number of all delivered pizzas (count)</td><td>${count_pizza}</td></tr>`
-    dataHtml += `<tr><td>Average delivery time</td><td>${ave_time.toFixed(2)} mins</td></tr>`
-    dataHtml += `<tr><td>Total sales in USD</td><td>${sum_sales.toFixed(2)}</td></tr>`
-    dataHtml += `<tr><td>Number of all feedback entries</td><td>${Object.keys(feedbackDat).length}</td></tr>`
-    dataHtml += `<tr><td>Number of feedback entries per quality category</td><td>Low: ${Object.keys(low).length} Medium: ${Object.keys(medium).length} High: ${Object.keys(high).length}</td></tr>`
+    let dataHtml = `<tr><td>Number of pizza deliveries</td><td>${deliveryDat.length}</td></tr>`;
+    dataHtml += `<tr><td>Number of all delivered pizzas (count)</td><td>${totalPizzas}</td></tr>`;
+    dataHtml += `<tr><td>Average delivery time</td><td>${avgTime.toFixed(2)} mins</td></tr>`;
+    dataHtml += `<tr><td>Total sales in USD</td><td>${totalSales.toFixed(2)}</td></tr>`;
+    dataHtml += `<tr><td>Number of all feedback entries</td><td>${feedbackDat.length}</td></tr>`;
+    dataHtml += `<tr><td>Number of feedback entries per quality category</td><td>Low: ${lowCount} Medium: ${mediumCount} High: ${highCount}</td></tr>`;
 
-    console.log(dataHtml)
     tableEle.innerHTML = dataHtml;
-
-
 }
+
 function createVisualization() {
+    let areaSelectBox = document.getElementById("area-category");
+    let areaValue = areaSelectBox.options[areaSelectBox.selectedIndex].value;
 
-    let areaselectBox = document.getElementById("area-category");
-    let areaValue = areaselectBox.options[areaselectBox.selectedIndex].value;
+    let typeSelectBox = document.getElementById("order-type-category");
+    let typeValue = typeSelectBox.options[typeSelectBox.selectedIndex].value;
 
-    let typeselectBox = document.getElementById("order-type-category");
-    let typeValue = typeselectBox.options[typeselectBox.selectedIndex].value;
-
-    let feedbackDat = feedbackData;
-    let deliveryDat = deliveryData;
-
-    loadTable(feedbackDat, deliveryDat);
-    renderBarChart(deliveryDat)
-
-    dataFiltering(areaValue, typeValue, deliveryDat, feedbackDat)
-
-
-
-    console.log(areaValue)
-    console.log(typeValue)
+    dataFiltering(areaValue, typeValue, deliveryData, feedbackData);
 }
